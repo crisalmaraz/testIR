8633b7ea03e6514d87cd3368f2ed8514163df2a4
crisalmaraz <crisalmaraz@outlook.com>
2019-09-10 18:15:36 -0500

    se agrego nueva ruta

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
